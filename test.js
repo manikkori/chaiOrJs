422b5129938b13b412fa34bf38c248773ede052f
Manik kori <manikkori697@gmail.com>
2026-06-22 10:21:01 +0000

    test nodeJs in codeSpaces

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-console.log("manik");
+let city = "Hapud"
+const cityPinCode = 245101;
+var state = "UP"
+
+
+city = "Hapur"
+//cityPinCode = 224422 //dont allow
+
+
+
+//console.log(city);
+console.table([city , cityPinCode , state] )
