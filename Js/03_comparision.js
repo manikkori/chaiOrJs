a075208732a38be03046ece18ab4dcdb36764a45
Manik kori <manikkori697@gmail.com>
2026-06-24 14:36:57 +0000

    comparision Op

---
+++ Js/03_comparision.js
@@ -0,0 +1,17 @@
+console.log(2>1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+console.log(2>=1);
+console.log(2<=1);
+
+console.log("2">1);
+
+console.log("2"==2);//true
+console.log("2"===2);//false
+
+console.log("2"!=2);//false
+console.log("2"!==2);//true
+
+
+
