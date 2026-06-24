d24ff5724042525360b895b67d8417c99328084a
Manik kori <manikkori697@gmail.com>
2026-06-24 09:43:09 +0000

    conversionNOpration

--- Js/02_conversionOperation.js
+++ Js/02_conversionOperation.js
@@ -1,28 +1,44 @@
+//1 .conversion---------------------------
+
 let num = "33"
 // let num = "33aabc"
 
-console.log(typeof num);
+// console.log(typeof num);
 
 let valueInNum = Number(num)
-console.log(typeof valueInNum);
+// console.log(typeof valueInNum);
 
-console.log(valueInNum);
+// console.log(valueInNum);
 // ------------------------------------------------
 
 let isLoggedIn = 0;
 
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //-------------------------------------------------
 
 let marks = 99;
 
 let numToStringMarks = String(marks)
-console.log(numToStringMarks);
-console.log(typeof numToStringMarks);
+// console.log(numToStringMarks);
+// console.log(typeof numToStringMarks);
 
 
+
+//2. operations-------------------------------------
+
+
+
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+let str1 = "Hello!, "
+let str2 = "Manik"
+let str3 = str1 +str2
+console.log(str3);
 
 
